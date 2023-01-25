e1f5bac7f9d2d06b070d78bf5d303a5b3f08d54a
Elisa Capololo <elisacapololo2017@gmail.com>
2023-01-25 20:31:03 +0000

    organize the code

--- app/src/main/java/com/example/login_and_registration_ui/InfoActivity.java
+++ app/src/main/java/com/example/login_and_registration_ui/InfoActivity.java
@@ -2,7 +2,11 @@ package com.example.login_and_registration_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        MaterialButton buttonNext = findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(InfoActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
     }
 }
