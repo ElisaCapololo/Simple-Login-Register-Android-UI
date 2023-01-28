9c84d2be1d9e16a96d6b174830a6d64764e2010a
Elisa Capololo <elisacapololo2017@gmail.com>
2023-01-28 16:56:29 +0000

    screen transition

--- app/src/main/java/com/example/login_and_registration_ui/LoginActivity.java
+++ app/src/main/java/com/example/login_and_registration_ui/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.login_and_registration_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView textView = findViewById(R.id.questionSignin);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/login_and_registration_ui/RegisterActivity.java
+++ app/src/main/java/com/example/login_and_registration_ui/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.login_and_registration_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.question);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
